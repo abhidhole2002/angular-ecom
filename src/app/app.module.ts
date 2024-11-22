c9314d7b16049575fd56455ccf77512419afe938
Abhishek Dhole <abhishekdhole60@gmail.com>
2024-11-22 23:41:27 +0530

    created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { OrderComponent } from './pages/admin/order/order.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
+import { CartComponent } from './pages/website/cart/cart.component';
+import { CheckoutComponent } from './pages/website/checkout/checkout.component';
+import { OrdersComponent } from './pages/website/orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    OrderComponent,
+    ProductsComponent,
+    LandingComponent,
+    CartComponent,
+    CheckoutComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
